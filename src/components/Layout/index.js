1feed2cfe9b7b32a4941526537308a2e88984fb4
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-10-25 00:18:23 +0100

    Add sidebar content

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,10 @@
+import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () => {
-    return <>Hello</>
+    return <div className='App'>
+        <Sidebar />
+    </div>
 }
 
 export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,84 @@
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSub from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+
+import './index.scss'
+import { faGithub, faLinkedin, faSkype, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+        <img src={LogoSub} alt="Slobodan" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/khouloudhaddadamamou/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4c" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/khouloudhaddad"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="skype:live:khouloud" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faSkype}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
