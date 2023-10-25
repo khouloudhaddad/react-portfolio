0cb45dfcbfe343eb516ca87ae8f6f01d07eb9655
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-10-25 01:18:12 +0100

    About me page & animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,75 @@
 import './index.scss'
 import LogoTitle from '../../assets/images/logo-s.png'
+import Logo from './Logo'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import AnimatedLetters from '../../components/AnimatedLetters'
+import { useEffect } from 'react'
 const Home = () => {
+  const [letterClass, setletterClass] = useState('text-animate')
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ];
+  
+  const timeout = async () => {
+    return setTimeout(() => {
+      setletterClass('text-animate-hover')
+    }, 4000)
+  }
+
+  useEffect(() => {
+    timeout()
+  }, [])
+
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          Hi,
-          <br />
-          I'm
-        <img src={LogoTitle} alt="Full Stack Developer" />
-        lobodan
-        <br />
-          Full Stack Developer
-        </h1>
-        <h2>PHP Senior Developer / Frontend developer / Team leader</h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-    </div>
+
+    </>
   )
 }
 
